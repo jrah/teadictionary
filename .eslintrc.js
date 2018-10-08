7c9bdbb2df433aac513fded8a4e4c5c8490fecf0
jrah <jjames.home@gmail.com>
2018-10-08 05:28:43 +0100

    Skip prettify, essential vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,17 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended'
+    'plugin:vue/essential',
+    // 'plugin:prettier/recommended'
   ],
   // required to lint *.vue files
   plugins: [
     'vue',
-    'prettier'
+    // 'prettier'
   ],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
