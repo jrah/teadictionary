eb352ca91c42c07475684b7a7e7437bc5b95c5c2
jrah <jjames.home@gmail.com>
2018-10-16 17:10:18 +0100

    Add modal component with css grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,86 @@
 <template>
   <section>
 
-<div class="mwc ph5-ns ph4 pv4">
-  <h1>Tea Dictionary</h1>
-  <no-ssr>
-    <auto-responsive class="center"
-      v-bind="options"
-    >
-      <div v-for="tea in teas" :key="tea.id" :style="getStyleList(tea)" class="item">
-        <h2>{{tea.title}}</h2>
-        <p>{{tea.truncated_copy}}</p>
-        {{tea.image}}
+<div class="ph5-ns ph4 pv4">
+  <div class="mwc center">
+    <h1>Tea Dictionary</h1>
+      <div class="grid-layout">
+        <span v-for="tea in teas" :key="tea.id" class="grid-item cover bg-center pointer white" v-bind:style="[ tea.image ? { 'background-image': 'url(' + tea.image + ')' } : { 'background-color': '#25324e' } ]">
+          <button type="button" @click="showModal">Open Modal</button>
+          <modal
+            v-show="isModalVisible"
+            @close="closeModal">
+          </modal>
+          <h2  class="f3">{{tea.title}}</h2>
+          <!-- <img v-if="tea.image" class="work-thumb" sizes="100vw" :src="tea.image" @error="imageLoadError($event)"> -->
+        </span>
       </div>
-    </auto-responsive>
-  </no-ssr>
+  </div>
 </div>
 
   </section>
 </template>
 
 <script>
+import modal from '~/components/Modal.vue';
 export default {
   components: {
-
-  },
-
+    modal
+   },
   async asyncData({ app }) {
     return {
       teas: await app.$content('./teas').getAll()
     }
   },
-  data() {
-   return {
-     loading: true,
-     teas: [],
-     options: {
-       itemMargin: 10,
-       containerWidth: 1299,
-       itemClassName: 'item',
-       gridWidth: 100,
-       transitionDuration: '.5'
-     }
-   }
- },
   methods: {
-    getStyleList(i) {
-      return {
-        width: `${i.w === 'w1' ? 100 : 250}px`,
-        height: `${i.w === 'w1' ? 100 : 250}px`
-      };
+    imageLoadError () {
+      console.log(this + 'Image failed to load');
+    },
+    showModal () {
+      this.isModalVisible = true;
+    },
+    closeModal () {
+      this.isModalVisible = false;
+    }
+  },
+  data () {
+    return {
+      isModalVisible: false
     }
   }
 }
 </script>
 
-<style lang="scss">
+
+
+<style scoped lang="scss">
+@import "assets/scss/mixins";
+@import "./node_modules/tachyons-sass/scss/variables";
+.grid-layout {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    grid-gap: 10px;
+    grid-auto-rows: minmax(180px, auto);
+    grid-auto-flow: dense;
+    padding: 10px;
+    transition: all 1s;
+}
+
+.grid-item {
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.span-2 {
+    grid-column-end: span 2;
+    grid-row-end: span 2;
+}
+
+.span-3 {
+    grid-column-end: span 3;
+    grid-row-end: span 4;
+}
 
 </style>
