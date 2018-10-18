f2a15bdf567e3d5db4efd5253a9e803922b1e7d8
jrah <jjames.home@gmail.com>
2018-10-18 22:21:48 +0100

    Stylise index

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.title,
+    title: this.title || pkg.title,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <section>
 
-<div class="ph5-ns ph4 pv4">
+<div class="ph5-ns ph4 pv3 bg-near-white">
+  <div class="h1 w4">
+    <img src="~assets/images/logo.svg" alt="Logo">
+  </div>
   <div class="mwc center">
-    <h1>Tea Dictionary</h1>
-      <div class="grid-layout">
+    <div class="mw7 center tc mwc ph5-ns ph4 pb5-ns pt4">
+      <h1 class="mt0 f2 mb3 lh-title">Tea Dictionary</h1>
+      <p class="lh-copy ma0">Artisan loose leaf tea curators DiversiTea in collaboration with the famous South Korean speciality kitchen KOPAN have worked to curate four different types of amazing teas that compliment and reflect the characteristics of Korean cuisine.</p>
+    </div>
+    <div class="relative">
+      <div class="grid-layout bg-white pv3 mh3 absolute left-0 right-0" style="top: -3rem;">
         <div v-for="(tea, index) in teas" :key="index" :name=" '' + index " @click="showModal(tea)" class="grid-item cover bg-center pointer white grow" v-bind:style="[ tea.image ? { 'background-image': 'url(' + tea.image + ')' } : { 'background-color': '#25324e' } ]">
 
           <h2  class="f3">{{tea.title}}</h2>
@@ -18,6 +25,7 @@
           <!-- <img slot="image" v-if="selectedTea.image" :src="selectedTea.image" alt=""> -->
         </modal>
       </div>
+    </div>
   </div>
 </div>
 
@@ -29,6 +37,11 @@ import modal from '~/components/Modal.vue';
 export default {
   components: {
     modal
+  },
+  data () {
+    return {
+      title: 'Tea Dictionary'
+    }
   },
   async asyncData({ app }) {
     return {
@@ -64,8 +77,8 @@ export default {
 .grid-layout {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
-    grid-gap: 10px;
-    grid-auto-rows: minmax(180px, auto);
+    grid-gap: .75em;
+    grid-auto-rows: minmax(10em, auto);
     grid-auto-flow: dense;
     padding: 10px;
     transition: all 1s;
