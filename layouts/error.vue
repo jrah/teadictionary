7057f06ae98137edaea137a78b1c0e2c76825a3a
jrah <jjames.home@gmail.com>
2018-10-26 20:29:44 +0100

    Add basic error page

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="errorPage">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occured: {{error}}</h1>
+    <nuxt-link to="/">Return</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blog' // you can set a custom layout for the error page
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
