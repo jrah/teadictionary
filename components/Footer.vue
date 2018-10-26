0665405b582c83a59b1fb022c1314c24a21392cf
jrah <jjames.home@gmail.com>
2018-10-26 17:16:41 +0100

    Alter footer colour contrast

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
-  <footer class="bg-navy white ">
+  <footer>
   <div class="mwc ph5-ns ph4 pv4 flex justify-between items-center">
       <small class="db fw8">© 2018 {{site.title_global}} </small>
       <ul class="pa0 ma0 list flex justify-between">
         <li v-for="(icon, index) in icons" :key="index" :name=" '' + index " class="dib mh2">
-          <a v-bind:href="icon.href" class="white">
+          <a v-bind:href="icon.href" class="near-black">
             <no-ssr>
               <v-icon v-bind:name="'brands/' + icon.type" scale="2" />
             </no-ssr>
