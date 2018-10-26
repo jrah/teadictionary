387d9ab04700c89b8201544f072436bb6393f657
jrah <jjames.home@gmail.com>
2018-10-26 20:44:54 +0100

    Remove one honeypot

--- components/Post.vue
+++ components/Post.vue
@@ -1,7 +1,6 @@
 <template>
 
       <form name="contact" action="" method="post" netlify-honeypot="bot-field" netlify>
-        <input type="hidden" name="contact" value="contact" />
         <p style="display: none;">
           <label>Don’t fill this out: <input name="bot-field"></label>
         </p>
