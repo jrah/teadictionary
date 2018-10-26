2ab0f037520a527c2ed6306db7834ba7b8510c92
jrah <jjames.home@gmail.com>
2018-10-26 22:11:51 +0100

    Remove one line elements from within form

--- components/Post.vue
+++ components/Post.vue
@@ -1,10 +1,6 @@
 <template>
 
       <form name="contact" action="/thanks" method="post" netlify-honeypot="bot-field" netlify>
-        <input type="hidden" name="contact" value="contact" />
-        <p style="display: none;">
-          <label>Don’t fill this out: <input name="bot-field"></label>
-        </p>
 
         <div class="form-group mb3">
           <label for="f_name" class="near-black lh-copy db mb1">Your name</label>
