aa79a3b97c7b7eadc2b8153cca628b59b0ddb19f
jrah <jjames.home@gmail.com>
2018-10-26 23:02:41 +0100

    Add hidden input, remove netlify option

--- components/Post.vue
+++ components/Post.vue
@@ -1,7 +1,7 @@
 <template>
 
-      <form name="contact" action="" method="post" netlify-honeypot="bot-field" netlify>
-        <input type="hidden" name="contact" value="contact" />
+      <form name="contact" action="" method="post" netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
         <p style="display: none;">
           <label>Don’t fill this out: <input name="bot-field"></label>
         </p>
